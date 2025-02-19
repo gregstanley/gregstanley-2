0958ae5f02ae31d029f540009f875940a33ae3fb
Greg Stanley <24586962+gregstanley@users.noreply.github.com>
2025-02-19 10:31:05 +0000

    Update packages
    Mainly to test GitHub Desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.html", "./src/**/*.ts", "./src/**/*.scss"],
   theme: {
     extend: {},
   },
