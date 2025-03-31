372bed0f971423dccdc722a5affdbabc59c208ae
Greg Stanley <24586962+gregstanley@users.noreply.github.com>
2025-03-31 17:10:02 +0100

    Add some styling and images (disable husky)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,43 @@ import { OtpComponent } from './otp/otp.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ClickerComponent, MastermindComponent, OtpComponent],
+  styleUrl: 'app.component.scss',
   template: `
-    <div class="max-w-prose flex flex-col mx-auto">
-      <header class="py-4">
-        <h1>Angular playground</h1>
+    <div class="fullscreen-bg"></div>
+    <div class="h-full flex flex-col mx-auto max-w-prose">
+      <header class="py-4 bg-sky-900 p-8 opacity-80 text-white">
+        <h1>
+          <img
+            src="/images/angular-full.svg"
+            height="3rem"
+            class="h-12"
+            alt="Angular logo"
+          />Playground
+        </h1>
       </header>
-      <div class="pb-4">
-        <app-clicker></app-clicker>
+      <div class="flex-1 bg-slate-200 p-8 opacity-90">
+        <div class="pb-4">
+          <app-clicker></app-clicker>
+        </div>
+        <div class="pb-4">
+          <app-mastermind></app-mastermind>
+        </div>
+        <div class="pb-4">
+          <app-otp></app-otp>
+        </div>
+        <router-outlet />
       </div>
-      <div class="pb-4">
-        <app-mastermind></app-mastermind>
-      </div>
-      <div class="pb-4">
-        <app-otp></app-otp>
-      </div>
-      <router-outlet />
+      <footer class="bg-slate-200 p-8 text-xs opacity-50">
+        <p>
+          <a
+            href="https://www.pexels.com/photo/purple-and-pink-diamond-on-blue-background-5011647/"
+            >Background photo by Rostislav Uzunov</a
+          >
+        </p>
+      </footer>
     </div>
   `,
 })
 export class AppComponent {
-  title = 'angular-starter';
+  title = 'Angular playground';
 }
